2d33885f75af3e2763bc8f5a2cb46c37a5da7859
prince nyarko <princenyarko360@gmail.com>
2025-12-01 21:37:26 +0000

    Modify ccounts.py with User model

--- school_management_system/accounts/models.py
+++ school_management_system/accounts/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)

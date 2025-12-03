f6aa0142176612c3c1aecc973a1073e6dfeed9d4
prince nyarko <princenyarko360@gmail.com>
2025-12-03 19:57:44 +0000

    Modify accounts.py with CustomUser model

--- school_management_system/accounts/models.py
+++ school_management_system/accounts/models.py
@@ -1,8 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+class CustomUser(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser with unique email and role fields.
+    """
     email = models.EmailField(unique=True)
+  # The role field specifies the user's role in the system, ('student', 'teacher', 'admin')
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
+    ]
     role = models.CharField(max_length=100)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+

0436d4adb48d30f8ad6ed5c806abe0b9bb33d428
prince nyarko <princenyarko360@gmail.com>
2025-12-03 22:30:29 +0000

    Modify admin.py to register custom user

--- school_management_system/accounts/admin.py
+++ school_management_system/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# @admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('first_name', 'last_name', 'email', 'role')
+    list_filter = ('role')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    
+admin.site.register(CustomUser, CustomUserAdmin)

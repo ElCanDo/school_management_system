44ca7a2d825bc716ab068fdcbe4f875e79dc4d98
prince nyarko <princenyarko360@gmail.com>
2025-12-12 20:37:36 +0000

    Modify admin.py and models.py

--- school_management_system/students/admin.py
+++ school_management_system/students/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Student
 
 # Student Admin Configuration
-class StudentAdmin(admin.ModelAdmin):# Custom admin configuration for Student model
-    list_display = ("name", "age", "enrollment_date", "gender", "grade") # Fields to display in admin list view
-    search_fields = ("name", "grade") # Fields to search in admin
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "enrollment_date", "gender", "grade") 
 
-admin.site.register(Student, admin.ModelAdmin) # Registering Student model with custom admin configuration
+admin.site.register(Student, UserAdmin) 
--- school_management_system/students/models.py
+++ school_management_system/students/models.py
@@ -1,5 +1,5 @@
-from django.db import models # Importing models module from django.db
-from django.core.validators import RegexValidator # RegexValidator for validating phone numbers
+from django.db import models 
+from django.core.validators import RegexValidator 
 
 # Student model definition
 class Student(models.Model):
@@ -7,32 +7,32 @@ class Student(models.Model):
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
-    ]# Choices for gender field
+    ]
 
-    # Inheritting from custom user model defined in 'accounts' app
+    
     user = models.OneToOneField(
         'accounts.CustomUser',
         on_delete=models.CASCADE,
         related_name='student_profile'
-    )#Link Student to CustomUser model
+    
 
-    date_of_birth = models.DateField()# Date of birth of the student
-    mother_full_name = models.CharField(max_length=200)# Full name of the mother
+    date_of_birth = models.DateField()
+    mother_full_name = models.CharField(max_length=200)
     
     mother_contact = models.CharField(max_length=15, 
                                       unique=True, 
-                                      validators=[RegexValidator(r'^\+?\d{9,15}$')]) # Validates phone number format
+                                      validators=[RegexValidator(r'^\+?\d{9,15}$')]) 
     
-    father_full_name = models.CharField(max_length=200)# Full name of the father
+    father_full_name = models.CharField(max_length=200)
     father_contact = models.CharField(max_length=15, 
                                       unique=True, 
-                                      validators=[RegexValidator(r'^\+?\d{9,15}$')] # Validates phone number format
+                                      validators=[RegexValidator(r'^\+?\d{9,15}$')] 
         )
-    home_address = models.TextField()# Student's residential address
-    medical_allergies = models.TextField()# Any known medical allergies
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)# Gender of the student
-    enrollment_date = models.DateField(auto_now_add=True)# Date when the student was enrolled
-    grade = models.ForeignKey('classrooms.classroom', on_delete=models.SET_NULL, null=True, related_name='students')# Grade level of the student
+    home_address = models.TextField()
+    medical_allergies = models.TextField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    enrollment_date = models.DateField(auto_now_add=True)
+    grade = models.ForeignKey('classrooms.classroom', on_delete=models.SET_NULL, null=True, related_name='students'
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - ({self.role})"

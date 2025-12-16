9f7f6c81fab391da153bd21afcd493a66655073e
prince nyarko <princenyarko360@gmail.com>
2025-12-16 21:07:12 +0000

    Modify views.py to Include authentication and permission classes for all views

--- school_management_system/school_api/views.py
+++ school_management_system/school_api/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions,authentication
-from . serializers import (CustomUserSerializer, 
+from rest_framework import viewsets, permissions
+from .serializers import (CustomUserSerializer, 
                            ClassroomSerializer, 
                            TeacherSerializer, 
                            StudentSerializer, 
@@ -11,37 +10,30 @@ from .models import CustomUser, Classroom, Teacher, Student, Enrollment, Teacher
 """Viewsets For all the Models"""
 
 class CustomUserViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = CustomUser.objects.all() 
-    serializer_class = CustomUserSerializer()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 class ClassroomViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = Classroom.objects.all()
     serializer_class = ClassroomSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 class TeacherViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = Teacher.objects.all() 
     serializer_class = TeacherSerializer
 
+
 class StudentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = Student.objects.all() 
     serializer_class = StudentSerializer
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 class TeacherAssignViewSet(viewsets.ModelViewSet):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
     queryset = TeacherAssign.objects.all()
     serializer_class = TeacherAssignSerializer
+    permission_classes = [permissions.IsAdminUser]
